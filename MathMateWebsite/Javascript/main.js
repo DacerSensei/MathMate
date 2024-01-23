51d3a139d255419594b7ae6d356cd809c9a9afa9
Dave Orijuela <daveorijuela@yahoo.com>
2024-01-23 22:01:30 +0800

    Updated website

--- MathMateWebsite/Javascript/main.js
+++ MathMateWebsite/Javascript/main.js
@@ -519,14 +519,29 @@ async function StudentQuiz() {
                         const editButton = document.createElement("button");
                         editButton.classList.add('Button-Yellow-Icon', 'modal-trigger');
                         editButton.title = "Edit";
-                        editButton.setAttribute('data-target', 'Edit-Modal');
+                        editButton.setAttribute('data-target', 'Add-Modal');
                         editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="pointer-events: none;"><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z"/></svg>';
+
+                        editButton.addEventListener('click', function () {
+                            var modalId = this.dataset.target;
+                            var modal = document.getElementById(modalId);
+                            console.log(modal);
+                            modal.style.display = 'flex';
+
+                            // When the user clicks on the close button, close the modal
+                            var closeBtn = modal.querySelector('.modal-close');
+                            closeBtn.addEventListener('click', function () {
+                                modal.style.display = 'none';
+                                document.getElementById("quiz-form").reset();
+                            });
+                        });
 
                         const deleteButton = document.createElement("button");
                         deleteButton.className = "Button-Red-Icon";
                         deleteButton.title = "Delete";
                         deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="pointer-events: none;"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>';
                         deleteButton.style.marginLeft = "5px";
+
                         // const disableButton = document.createElement("button");
                         // disableButton.className = "Button-Red-Icon";
                         // disableButton.style = "margin-left: 4px;"
@@ -583,13 +598,13 @@ async function StudentLesson() {
                         editButton.title = "Edit";
                         editButton.setAttribute('data-target', 'Edit-Modal');
                         editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="pointer-events: none;"><path d="M14.06,9L15,9.94L5.92,19H5V18.08L14.06,9M17.66,3C17.41,3 17.15,3.1 16.96,3.29L15.13,5.12L18.88,8.87L20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18.17,3.09 17.92,3 17.66,3M14.06,6.19L3,17.25V21H6.75L17.81,9.94L14.06,6.19Z"/></svg>';
-                        
+
                         const deleteButton = document.createElement("button");
                         deleteButton.className = "Button-Red-Icon";
                         deleteButton.title = "Delete";
                         deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="pointer-events: none;"><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>';
                         deleteButton.style.marginLeft = "5px";
-                        
+
                         // const disableButton = document.createElement("button");
                         // disableButton.className = "Button-Red-Icon";
                         // disableButton.style = "margin-left: 4px;"
--- MathMateWebsite/Javascript/student-quiz.js
+++ MathMateWebsite/Javascript/student-quiz.js
@@ -13,65 +13,11 @@ const SecondaryApp = initializeApp(FirebaseConfig, "SecondaryApp");
 const SecondaryAuth = getAuth(SecondaryApp);
 
 document.getElementById("AddProblem").addEventListener("click", async (e) => {
-    const sectionElement = document.getElementById("quiz-form").querySelector("section");
-    // Create a new input element
-    // Assuming you have a reference to the parent container
-    const previousTitle = sectionElement.children[sectionElement.children.length - 3].querySelector("p").textContent;
-    const previousInt = parseInt(previousTitle.charAt(previousTitle.length - 1))
+    CreateQuestion();
+});
 
-    // Create a div element with the class "quiz-problem"
-    const quizProblemDiv = document.createElement("div");
-    quizProblemDiv.classList.add("quiz-problem");
-
-    // Create a paragraph element with the text "Question 1"
-    const questionParagraph = document.createElement("p");
-    questionParagraph.textContent = "Question " + (previousInt + 1).toString();
-
-    // Create the first input div with the class "Solid-Textbox-Red"
-    const firstInputDiv = document.createElement("div");
-    firstInputDiv.classList.add("Solid-Textbox-Red");
-
-    // Create an icon element with the class "fa-solid fa-signature"
-    const firstIcon = document.createElement("i");
-    firstIcon.classList.add("fa-solid", "fa-signature");
-
-    // Create the first input element with the type "text" and the id "quiz-title"
-    const firstInput = document.createElement("input");
-    firstInput.type = "text";
-    firstInput.id = "quiz-title";
-    firstInput.placeholder = "Enter your problem";
-    firstInput.required = true;
-
-    // Append the icon and input to the first input div
-    firstInputDiv.appendChild(firstIcon);
-    firstInputDiv.appendChild(firstInput);
-
-    // Create the second input div with the class "Solid-Textbox-Red"
-    const secondInputDiv = document.createElement("div");
-    secondInputDiv.classList.add("Solid-Textbox-Red");
-
-    // Create an icon element with the class "fa-solid fa-signature"
-    const secondIcon = document.createElement("i");
-    secondIcon.classList.add("fa-solid", "fa-signature");
-
-    // Create the second input element with the type "text" and the id "quiz-title"
-    const secondInput = document.createElement("input");
-    secondInput.type = "text";
-    secondInput.id = "quiz-title";
-    secondInput.placeholder = "Enter your solution";
-    secondInput.required = true;
-
-    // Append the icon and input to the second input div
-    secondInputDiv.appendChild(secondIcon);
-    secondInputDiv.appendChild(secondInput);
-
-    // Append all elements to the main quiz problem div
-    quizProblemDiv.appendChild(questionParagraph);
-    quizProblemDiv.appendChild(firstInputDiv);
-    quizProblemDiv.appendChild(secondInputDiv);
-
-    // Insert the new input element at the specified position
-    sectionElement.insertBefore(quizProblemDiv, sectionElement.children[sectionElement.children.length - 2]);
+document.getElementById("CreateData").addEventListener("click", () => {
+    ClearQuestions();
 });
 
 document.getElementById("quiz-form").addEventListener("submit", async (e) => {
@@ -97,14 +43,58 @@ document.getElementById("quiz-form").addEventListener("submit", async (e) => {
     await set(ref(Database, "teachers/" + parsedData.uid + '/Quiz/' + title), quizData);
     document.getElementById("quiz-form").reset();
     document.querySelector('.modal-close').click();
+
     ShowPopup("You just created a new quiz");
+    for (let i = firstIndex + 1; i < lastIndex; i++) {
+        sectionElement.children[4].remove();
+    }
     document.getElementById("table-body").innerHTML = "";
+
     await StudentQuiz();
     HideLoading();
 });
 
 const table = document.getElementById("myTable");
 table.addEventListener("click", async (event) => {
+    if (event.target && event.target.matches(".Button-Yellow-Icon")) {
+        const sectionElement = document.getElementById("quiz-form").querySelector("section");
+
+        const firstIndex = 3;
+        const lastIndex = sectionElement.children.length - 2;
+
+        for (let i = firstIndex + 1; i < lastIndex; i++) {
+            sectionElement.children[4].remove();
+        }
+
+        const row = event.target.closest("tr");
+        const hiddenInput = row.querySelector("input[type='hidden']");
+        const id = hiddenInput.value;
+        const parsedData = JSON.parse(localStorage.getItem('userData'));
+        document.getElementById("quiz-form").querySelector("section").children[2].querySelector("input").value = id;
+        try {
+            await get(child(ref(Database), 'teachers/' + parsedData.uid + '/Quiz/' + id)).then(async (quizSnapshot) => {
+                const data = quizSnapshot.val();
+                if (data) {
+                    for (let i = 0; i < Object.keys(data).length - 1; i++) {
+                        CreateQuestion();
+                    }
+                    const sectionElement = document.getElementById("quiz-form").querySelector("section");
+                    let firstIndex = 3;
+                    for (const [key, values] of Object.entries(data)) {
+                        const parsedData = JSON.parse(localStorage.getItem('userData'));
+                        const questionAndSolution = sectionElement.children[firstIndex].querySelectorAll("input");
+                        questionAndSolution[0].value = values.problem;
+                        questionAndSolution[1].value = values.solution;
+                        firstIndex++;
+                    }
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
     if (event.target && event.target.matches(".Button-Red-Icon")) {
         const row = event.target.closest("tr");
 
@@ -120,3 +110,62 @@ table.addEventListener("click", async (event) => {
         }
     }
 });
+
+function CreateQuestion() {
+    const sectionElement = document.getElementById("quiz-form").querySelector("section");
+    const previousTitle = sectionElement.children[sectionElement.children.length - 3].querySelector("p").textContent;
+    const previousInt = parseInt(previousTitle.charAt(previousTitle.length - 1))
+
+    const quizProblemDiv = document.createElement("div");
+    quizProblemDiv.classList.add("quiz-problem");
+
+    const questionParagraph = document.createElement("p");
+    questionParagraph.textContent = "Question " + (previousInt + 1).toString();
+
+    const firstInputDiv = document.createElement("div");
+    firstInputDiv.classList.add("Solid-Textbox-Red");
+
+    const firstIcon = document.createElement("i");
+    firstIcon.classList.add("fa-solid", "fa-signature");
+
+    const firstInput = document.createElement("input");
+    firstInput.type = "text";
+    firstInput.id = "quiz-title";
+    firstInput.placeholder = "Enter your problem";
+    firstInput.required = true;
+
+    firstInputDiv.appendChild(firstIcon);
+    firstInputDiv.appendChild(firstInput);
+
+    const secondInputDiv = document.createElement("div");
+    secondInputDiv.classList.add("Solid-Textbox-Red");
+
+    const secondIcon = document.createElement("i");
+    secondIcon.classList.add("fa-solid", "fa-signature");
+
+    const secondInput = document.createElement("input");
+    secondInput.type = "text";
+    secondInput.id = "quiz-title";
+    secondInput.placeholder = "Enter your solution";
+    secondInput.required = true;
+
+    secondInputDiv.appendChild(secondIcon);
+    secondInputDiv.appendChild(secondInput);
+
+    quizProblemDiv.appendChild(questionParagraph);
+    quizProblemDiv.appendChild(firstInputDiv);
+    quizProblemDiv.appendChild(secondInputDiv);
+
+    sectionElement.insertBefore(quizProblemDiv, sectionElement.children[sectionElement.children.length - 2]);
+}
+
+function ClearQuestions() {
+    const sectionElement = document.getElementById("quiz-form").querySelector("section");
+
+    const firstIndex = 3;
+    const lastIndex = sectionElement.children.length - 2;
+
+    for (let i = firstIndex + 1; i < lastIndex; i++) {
+        sectionElement.children[4].remove();
+    }
+}
